bc21af72cd1d74d04a81d18307da7c79dfa559a5
Zhiming Zhang <zhiming41314@gmail.com>
2025-03-09 18:49:37 +0000

    Used new dialect for SQL: Replaced sqlite using SQLAlchemy, functions needed to be implemented are denoted with "pass"

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,49 @@
-import sqlite3 as sql
-conn = sql.connect("VR3.db")
-cur = conn.cursor()
-cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-tables = cur.fetchall()
-for table in tables:
-            print(table[0])
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Game(db.model):
+    __tablename__ = 'GameInfo'
+    game_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    reference = db.Column(db.Text, )
+    reg = db.relationship("Game", backref='game')
+
+    def getReferenceURL(self, id):
+        pass
+
+class User(db.model):
+    __tablename__ = 'UserInfo'
+    phone = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.Text, nullable=False)
+    group = db.Column(db.Integer)
+    reg = db.relationship('User', backref='user')
+
+    def fetch_all_phone(self):
+        pass
+
+    def getGroupViaPlayer(self, user):
+        pass
+
+    def getAllPlayersViaGroup(self, grp):
+        pass
+
+    def getPlayerViaPhone(self, user):
+        pass
+
+    def removePlayer(self,user):
+        pass
+
+
+
+class Register(db.model):
+    __tablename__ = 'Register'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
+
+    def getPlayersPerGame(self, game):
+        pass
+
+    def getGamesPerPlayer(self, user):
+        pass
--- backend/initialisation.py
+++ backend/initialisation.py
@@ -1,25 +1,30 @@
 import sqlite3 as sql
-conn = sql.connect("VR3.db")
-cur = conn.cursor()
+def initialise_db():
+    conn = sql.connect("VR3.db")
+    cur = conn.cursor()
 
 
 
-cur.execute('''
-CREATE TABLE IF NOT EXISTS UserInfo (
-    phone INTEGER PRIMARY KEY,
-    password TEXT UNIQUE NOT NULL,
-    class INT
-);''')
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS UserInfo (
+        phone INTEGER PRIMARY KEY,
+        password TEXT UNIQUE NOT NULL,
+        class INT
+    );''')
 
-cur.execute('''CREATE TABLE IF NOT EXISTS Register (
-    username TEXT NOT NULL,
-    game_id INTEGER NOT NULL,
-    FOREIGN KEY (username) REFERENCES UserInfo(phone),
-    FOREIGN KEY (game_id) REFERENCES GameInfo(game_id)
-);''')
-cur.execute('''CREATE TABLE IF NOT EXISTS GameInfo (
-    game_id INTEGER PRIMARY KEY,
-    reference TEXT NOT NULL
-);''')
-conn.commit()
-conn.close()
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS GameInfo (
+        game_id INTEGER PRIMARY KEY,
+        reference TEXT NOT NULL
+    );''')
+
+    cur.execute('''CREATE TABLE IF NOT EXISTS Register (
+            username TEXT NOT NULL,
+            game_id INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            FOREIGN KEY (username) REFERENCES UserInfo(phone),
+            FOREIGN KEY (game_id) REFERENCES GameInfo(game_id)
+        );''')
+
+    conn.commit()
+    conn.close()
